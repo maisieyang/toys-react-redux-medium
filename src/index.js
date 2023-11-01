387133ec250e4110c247c624e508305ca617c3ee
yangxiyue <yangxiyuebupt@gmail.com>
2023-11-01 14:12:33 +0800

    delete

--- src/index.js
+++ src/index.js
@@ -1,20 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// import { Provider } from 'react-redux';
-// import store from './app/store';
-// import { BrowserRouter as Router } from 'react-router-dom';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <Router>
-//         <App/>
-//       </Router>
-//     </Provider>
-//   </React.StrictMode>
-// );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
